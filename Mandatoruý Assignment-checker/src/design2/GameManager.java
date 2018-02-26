97b10901e64b67764118a76c70ca0b9a09b6ba09
Clara <Clara@Clara-Pc>
2018-02-26 18:44:04 +0100

    updating gameManager

--- Mandatoruý Assignment-checker/src/design2/GameManager.java
+++ Mandatoruý Assignment-checker/src/design2/GameManager.java
@@ -1,5 +1,6 @@
-package design2;
 
+package design2;
+//TEST
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,36 +12,68 @@ public class GameManager {
 	static int newPosition_y;
 
 	public static Coordinate[] playersMove() {
-		try {
-			System.out.println("Coordinate of piece to move");
-			System.out.println("Enter X: ");
-			position_x = scan.nextInt();
-			
-			System.out.println("Enter Y: ");
-			position_y = scan.nextInt();
-		}
-		catch(InputMismatchException e) {
-			System.out.println("ERROR. You can only input integers");
-		}
-			Coordinate oldPosition=new Coordinate(position_x,position_y);
-			
-		try {
-			System.out.println("New X position");
-			newPosition_x = scan.nextInt();
 
-			System.out.println("New Y position");
-			newPosition_y = scan.nextInt();
+		boolean t = true;
+		while(t) {
+			try {		
+				System.out.println("Coordinate of piece to move");
+				System.out.println("Enter X: ");
+
+				position_x = scan.nextInt();
+				t = false;
+			}
+			catch(InputMismatchException e) {
+				scan.nextLine();
+				System.out.println("ERROR. You can only input integers");
+
+			}
 		}
-		catch(InputMismatchException e) {
-			System.out.println("ERROR. You can only input integers");
+		t = true;
+		while(t) {
+			try {		
+				System.out.println("Enter Y: ");
+				position_y = scan.nextInt();
+				t = false;
+			}
+			catch(InputMismatchException e) {
+				scan.nextLine();
+				System.out.println("ERROR. You can only input integers");
+
+			}
 		}
-		
+		Coordinate oldPosition=new Coordinate(position_x,position_y);
+
+		t = true;
+		while(t) {
+			try {
+				System.out.println("New X position");
+				newPosition_x = scan.nextInt();
+				t = false;
+			}
+			catch(InputMismatchException e) {
+				System.out.println("ERROR. You can only input integers");
+				scan.nextLine();
+
+			}
+		}
+
+		t = true;
+		while(t) {
+			try {
+				System.out.println("New Y position");
+				newPosition_y = scan.nextInt();
+				t = false;
+			}
+			catch(InputMismatchException e) {
+				System.out.println("ERROR. You can only input integers");
+				scan.nextLine();
+			}
+		}
+
 		Coordinate newPosition=new Coordinate(newPosition_x,newPosition_y);		
-	
-	Coordinate[] move=new Coordinate[] {oldPosition,newPosition};
-	return move;
-	}
-	
-	
-}
 
+		Coordinate[] move=new Coordinate[] {oldPosition,newPosition};
+		return move;
+	}
+
+}
