202d816c79e7fc2abeae5ee19c2246f10bbb672e
Atli Olsen <Atli Olsen@DESKTOP-3UFSJRU>
2018-02-26 21:06:07 +0100

    Finishing Touch.. Comment bad code in old version

--- Mandatoruý Assignment-checker/src/Rules.java
+++ Mandatoruý Assignment-checker/src/Rules.java
@@ -7,25 +7,25 @@ public class Rules {
 		return false;
 	}
 
-	public static boolean diagonalMove(Piece piece, Position newPosition) {
-		Player player1=new Player("1");
-		Player player2=new Player("2");
-		return( 
+//	public static boolean diagonalMove(Piece piece, Position newPosition) {
+//		Player player1=new Player("1");
+//		Player player2=new Player("2");
+//		return( 
 				//testing if move is diagonal
-				((piece.getPosition().getX()%2==0 && newPosition.getX()%2!=0) ||
-						(newPosition.getX()%2==0 && piece.getPosition().getX()%2!=0)) &&
+//				((piece.getPosition().getX()%2==0 && newPosition.getX()%2!=0) ||
+//						(newPosition.getX()%2==0 && piece.getPosition().getX()%2!=0)) &&
 
 				//testing if move is forward
-				((piece.getOwner()==player1 && newPosition.getY()==piece.getPosition().getY()+1) ||
-						(piece.getOwner()==player2 && newPosition.getY()==piece.getPosition().getY()-1))
+//				((piece.getOwner()==player1 && newPosition.getY()==piece.getPosition().getY()+1) ||
+//						(piece.getOwner()==player2 && newPosition.getY()==piece.getPosition().getY()-1))
 
-				);
-	}
+//				);
+//	}
 	
-	public static boolean ownership(Game game, Piece piece) {
-		return
-		(game.getBoard().getBoard()[piece.getPosition().getY()][piece.getPosition().getX()]==game.getCurrentPlayer().toString());
-	}
+//	public static boolean ownership(Game game, Piece piece) {
+//		return
+//		(game.getBoard().getBoard()[piece.getPosition().getY()][piece.getPosition().getX()]==game.getCurrentPlayer().toString());
+//	}
 	
 	public static boolean isEmptySpace(Game game, Position position) {
 		return (game.getBoard().getBoard()[position.getY()][position.getX()]==" ");
